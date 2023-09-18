073d687d2be0db6c155c131f77e98672b0d4b0d1
denieldiniz <denieldiniz@gmail.com>
2023-09-17 23:03:50 -0300

    Finalizado HookUseReducer

--- src/components/HookUseReducer_origem.js
+++ src/components/HookUseReducer_origem.js
@@ -1,20 +1,20 @@
 import { useReducer, useState } from "react"
 
 const HookUseReducer = () => {
+  // 1 - começando com useReducer
   const [number, dispatch] = useReducer((state, action) => {
-    return Math.random(state).toFixed(2)
-  })
+    return Math.random(state)
+  }, 1)
 
   // 2 - avançando no useReducer
   const initialTasks = [
-    { id: 1, text: "fazer uma coisa" },
-    { id: 2, text: "Dar um xeru em minha branquinha" },
-    { id: 3, text: "Abraçar minhas cocadas" },
+    { id: 1, text: "Fazer alguma coisa" },
+    { id: 2, text: "Fazer outra coisa" },
   ]
 
   const taskReducer = (state, action) => {
     switch (action.type) {
-      case "ADD": {
+      case "ADD":
         const newTask = {
           id: Math.random(),
           text: taskText,
@@ -23,15 +23,11 @@ const HookUseReducer = () => {
         setTaskText("")
 
         return [...state, newTask]
-      }
 
-      case "DELETE": {
+      case "REMOVE":
         return state.filter((task) => task.id !== action.id)
-      }
-
-      default: {
+      default:
         return state
-      }
     }
   }
 
@@ -40,43 +36,41 @@ const HookUseReducer = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault()
-
     dispatchTask({ type: "ADD" })
   }
 
   const removeTask = (id) => {
-    dispatchTask({ type: "DELETE", id: id })
+    dispatchTask({ type: "REMOVE", id: id })
   }
 
   return (
     <div>
-      <hr />
-      <h2>UseReducer</h2>
+      <h2>useReducer</h2>
       <p>Número: {number}</p>
-      <button onClick={dispatch}>gerar número</button>
-
-      <h3>Tarefas:</h3>
+      <button onClick={dispatch}>Alterar número!</button>
       <form onSubmit={handleSubmit}>
         <input
           type="text"
           onChange={(e) => setTaskText(e.target.value)}
           value={taskText}
         />
-
         <input
           type="submit"
-          value="Enviar tarefa"
+          value="Add"
         />
       </form>
-
-      {tasks.map((task) => (
-        <li
-          key={task.id}
-          onDoubleClick={() => removeTask(task.id)}>
-          {task.text}
-        </li>
-      ))}
+      <ul>
+        {tasks.map((task) => (
+          <li
+            key={task.id}
+            onDoubleClick={() => removeTask(task.id)}>
+            {task.text}
+          </li>
+        ))}
+      </ul>
+      <hr />
     </div>
   )
 }
+
 export default HookUseReducer
